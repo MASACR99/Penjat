b5a6d7637c0a65b57728f0e61f7d2a545a1e14db
MASA <joangil99@gmail.com>
2021-02-10 17:16:02 +0100

    Start work on Tauler.java
    All methods in, need to fill in

--- penjat/src/com/jaume/penjat/Tauler.java
+++ penjat/src/com/jaume/penjat/Tauler.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jaume.penjat;
 
-/**
- *
- * @author masa
- */
 public class Tauler {
+    private char[] paraulaSecreta; //Same len, init = string user
+    private String[] paraulaEndevinada; //Same len, init = nulls
+    private int intents; //init = user
+    private int intents_total; //init = user
     
+    public Tauler(){
+        //Constructor buid
+    }
+
+    public char[] getParaulaSecreta() {
+        return paraulaSecreta;
+    }
+
+    public String[] getPalabraEndevinada() {
+        return paraulaEndevinada;
+    }
+
+    public int getIntents() {
+        return intents;
+    }
+    
+    public void inicialitzarPartida(String paraula, int intent){
+        //Inicialitzar paraulaEndevinada a tot null i longitud = paraule.length
+        //intents = intent
+        //intents_total = intent
+        //paraulaSecreta = paraula.toCharArray();
+    }
+    
+    public String imprimir(){
+        
+    }
+    
+    public String imprimirVides(){
+        //"Et queden 4 vides de 4"
+        //Retornar "Et queden intents d'un total de intents_total"
+    }
+    
+    public boolean hasGuanyat(){
+        
+    }
+    
+    public String verificar(String lletra){
+        //Comprovar només una lletra
+        //Incorrecte retorna: "LLetra incorrecte"
+        //Correcte retorna: "Lletra correcte, n'hi ha X"
+        //Correcte pero no lletra: "Aquesta lletra no hi és"
+    }
 }
