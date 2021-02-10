da4f7b76ebcc979add650dff00a2abd4d2535fce
MASA <joangil99@gmail.com>
2021-02-10 16:30:42 +0100

    Solved test api error
    Needed to manually add junit

--- penjat/test/test/TaulerTest.java
+++ penjat/test/test/TaulerTest.java
@@ -1,7 +1,7 @@
 package test;
 
 import com.jaume.penjat.Tauler;
-import org.junit.api.BeforeEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
